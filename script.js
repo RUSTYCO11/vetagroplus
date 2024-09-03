bccb66e683a15ddda41226beecfdd512629d8471
RUSTYCO11 <125678188+RUSTYCO11@users.noreply.github.com>
2024-09-03 09:10:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPrincipal = document.que", "Acessibilidade com tecnologias assistivas, comunidades online, facilitando o acesso ao conhecimento.rySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-pergunta");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
@@ -7,14 +7,51 @@ const textoResultado = document.querySelector(".textoResultado");
 const perguntas = [
     {
         enunciado: "informe o estado do seu animal a baixo!",
-            alternativas: ["baixo,médio ou alterado."]
+            alternativas: ["baixo, médio, grave ou alterado."]
         },
     {
         enunciado: "quanto tempo o animal vem representando esses sintomas?",
-        alternativas: ["", "Acessibilidade com tecnologias assistivas, comunidades online, facilitando o acesso ao conhecimento."]
+        alternativas: ["menos de uma semana, mais de duas semanas ou mais de um mês"]
     },
     {
-        enunciado: "Nos dias atuais percebemos que diversas alterações climáticas estão causando desastres naturais com uma intensidade que era esperada para daqui a 20 anos. Cada vez mais estão se confirmando episódios de desastres naturais devido ao aquecimento global. Esse cenário demanda o surgimento de novas profissões que venham a mitigar esse quadro. Para o seu futuro, você escolherá qual tipo de profissão?",
-        alternativas: ["Eu não acredito no aquecimento global é fake news! Eu me importo com uma profissão rentável!", "Eu acredito no aquecimento global e vou me focar em profissões tecnológicas que ajudem na mitigação!"]
+        enunciado: "informe qual e seu animal?",
+        alternativas: ["Bovino, Suino, Equino, Cabrino ou outros"]
     },
 ];
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta(){
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    textoResultado.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener('click', ()=> respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);        
+    }
+}
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacao + " ";
+    atual++
+    mostraPergunta(); 
+}
+mostraPergunta();
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "Em 2030...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
